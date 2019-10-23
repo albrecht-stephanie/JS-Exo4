d6db0fff8f76bf0d86f58927b0cb74231a6a491b
AS <albrecht.stephanie@outlook.fr>
2019-10-23 09:15:44 +0200

    demande du nom, prénom et ville avec boite de dialogue

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+var lastname = "Doe";
+var firstname = "John";
+var city = "New-York";
+
+const prenom = prompt("Entrer votre nom");
+alert(`Bonjour, ${lastName}`);
+const prenom = prompt("Entrer votre prénom");
+alert(`Bonjour, ${firstName}`);
+const prenom = prompt("Entrer votre prénom");
+alert(`Bonjour, ${city}`);
